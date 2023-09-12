d4ff5e763c83722fd0f22e21139da629ebfa87fa
tektechnologies <info@tektechnologies.com>
2023-09-12 13:20:25 -0500

    add pop and push demo for arrays

--- js/app.js
+++ js/app.js
@@ -2,7 +2,9 @@
 console.log('app js is connected!!!');
 
 //take in some user input
-let userAnswer = prompt('Is my favorite food pizza? Please respond with yes or no!');
+let userAnswer = prompt(
+  'Is my favorite food pizza? Please respond with yes or no!'
+);
 console.log(userAnswer);
 //       trim removes spaces and lower case will lowercase input.
 //clean up the input
@@ -11,7 +13,7 @@ let cleanUserAnswer = userAnswer.trim().toLowerCase();
 console.log('lowercase and no space:', cleanUserAnswer);
 
 //we are going to use if else conditions to determine a response to the user.
-if(cleanUserAnswer === 'yes' || cleanUserAnswer === 'y'){
+if (cleanUserAnswer === 'yes' || cleanUserAnswer === 'y') {
   alert('Your right! Pizza is great!');
 } else {
   alert('Your wrong pizza is my favorite!');
@@ -22,14 +24,10 @@ console.log(userName);
 console.log(typeof userAnswer);
 // USE A PARSE INT OR NUMBER TO CHANGE THE STRING INTO NUMBER.
 
-
-
-
 //short circuit.
 // if(true || false || true){
 //   then this will run 'in here'
 // }
-
 
 //Look at some data types : boolean, string, number,
 let myBooleenValueTrue = false;
@@ -47,68 +45,40 @@ if (myBooleenValueTrue && thirdBooleenValueTrue) {
   console.log('we hit the else everything was false....');
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //Introduction to arrays so we can store some stuff, strings, number, arrays, objects
 //learn some methods that we can use arrays to do stuff
 
 let emptyArray = [];
 console.log(emptyArray);
 //                  0,1,2,3,4,5 the 6th element is at index 5
-let quizAnswers = ['c','a','d','c','e','c'];
+let quizAnswers = ['c', 'a', 'd', 'c', 'e', 'c'];
 console.log(quizAnswers);
 
-
 //nested Arrays
-let createNested = [['a'],[[]],[{},5]];
+let createNested = [['a'], [[]], [{}, 5]];
 console.log(createNested);
-
 
 let nestedArray = [
   ['yes', 'y'], //0 index  1st element
 
   ['no', 'n'], // 1 index  2nd element
 
-  ['maybe', 'maybe not'] //2 index do not want a comma here.  3rd element.
+  ['maybe', 'maybe not'], //2 index do not want a comma here.  3rd element.
 ];
 console.log(nestedArray);
 
 // we can put different data types in our arrays.
 let mixedDataTypesArray = ['Harry', 'Potter', 10, true, 'Magic', {}];
-console.log('🚀 ~ file: app.js:90 ~ mixedDataTypesArray:', mixedDataTypesArray);
+console.log('mixedDataTypesArray:', mixedDataTypesArray);
 
+//how can we access things in our arrays.
+//                 0,   1,   2
+let myArrayOne = ['a', 'x', 'c'];
+console.log(myArrayOne[1]);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if (myArrayOne[1] === 'x') {
+  alert('you picked the 1st index which is the second element!');
+}
 
 /**
  * pop()
@@ -117,3 +87,23 @@ console.log('🚀 ~ file: app.js:90 ~ mixedDataTypesArray:', mixedDataTypesArray
  * what can we put in a js array
  * how to access an index in an array (how do we get it out again?)
  */
+
+let myArrayTwo = [1, 2, 3, 4, 5];
+console.log('myArrayTwo:', myArrayTwo);
+
+//this is how we can add an element to the end of our array.
+// use the push() method.
+
+myArrayTwo.push(6);
+console.log('myArrayTwo after the push:', myArrayTwo);
+
+myArrayTwo.pop();
+console.log('pop: ', myArrayTwo);
+
+let myArrayThree = [1, 2, 3, 4, 5];
+// dot operator
+myArrayThree.pop();
+myArrayThree.pop();
+myArrayThree.pop();
+
+console.log('pop(): ', myArrayThree);
